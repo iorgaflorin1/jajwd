f6667a7274184dd9d84561b9b33386a57be4f120
Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>
2018-09-08 20:07:09 +0300

    Java 9 Signed-off-by:Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>

--- JavaGroup8/Java9GheorghePacurar/src/calculator/Calculator.java
+++ JavaGroup8/Java9GheorghePacurar/src/calculator/Calculator.java
@@ -10,12 +10,25 @@ package calculator;
  * @author gheor
  */
 public class Calculator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    int rezultat = 0;
+    
+    public int aduna (int a, int b){
+        rezultat = a+b;
+        return rezultat;
     }
     
+    public int scade (int a, int b){
+        rezultat = a-b;
+        return rezultat;
+    }
+    
+    public int inmulteste(int a, int b){
+        rezultat = a*b;
+        return rezultat;
+    }
+    
+    public int imparte (int a, int b){
+        rezultat = a/b;
+        return  rezultat;
+    }
 }
