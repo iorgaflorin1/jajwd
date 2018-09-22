066ea0a28deaadda4c3f7670dfb7c3779784fd5a
Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>
2018-09-22 16:34:18 +0300

    Java 9 Autoservice homework Signed-off-by:Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>

---
+++ JavaGroup8/Java9GheorghePacurar/src/autoservice/Saleable.java
@@ -0,0 +1,22 @@
+/*
+ * Interface Saleable
+ */
+package autoservice;
+
+/**
+ *
+ * @author gheor
+ */
+public interface Saleable {
+    /**
+     @const REC_SALE_PRICE is recommended price to sell
+     */
+    public static final boolean SALEABLE = true;
+    
+    /**
+     * Method returns the value of item that implements the saleable interface.
+     * Class that implements the interface should implement this method.
+     @return method returns price for sale
+     */
+    public int getSalePrice();
+}
