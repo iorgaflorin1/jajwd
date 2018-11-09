e77c0caf149b0a9e54e9c1b11e2107542dfdadae
Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>
2018-11-09 17:49:04 +0200

    Merge origin/master Signed-off-by:Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com> Signed-off-by:Gheorghe Aurel Pacurar <gheorghe.pacurar@gmail.com>

--- JavaGroup8/Java9GheorghePacurar/src/tdd/Patrat.java
+++ JavaGroup8/Java9GheorghePacurar/src/tdd/Patrat.java
@@ -9,13 +9,14 @@ package tdd;
  */
 public class Patrat extends PatrulaterDreptunghic{
     
-    public static final String PATRAT= " equal";
+    public static final String PATRAT= " equal ";
     public static final String PATRAT_BEGIN= "SQUARE:";
     
     public String getDefinition(){
     
-        String definition = PATRAT_BEGIN+super.getDefinition().substring(0, 23)+PATRAT+super.getDefinition().substring(24);
+        String definition = PATRAT_BEGIN+super.getDefinition().substring(0, 25)+PATRAT+super.getDefinition().substring(26);
         return definition;
+        //return "";
     }
     
 }
