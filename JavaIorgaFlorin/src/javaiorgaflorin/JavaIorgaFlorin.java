6dd7aa11fe38f393f00e90ddedf1962ea5be2419
Florin <Florin@192.168.1.106>
2019-05-09 15:39:17 +0300

    This is my first version.

---
+++ JavaIorgaFlorin/src/javaiorgaflorin/JavaIorgaFlorin.java
@@ -0,0 +1,20 @@
+/*
+ * First class that will be versionated
+
+ */
+package javaiorgaflorin;
+
+/**
+ * First class that will be versionated
+ * @author Florin
+ */
+public class JavaIorgaFlorin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my first program.");
+    }
+    
+}
